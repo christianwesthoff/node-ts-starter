86b354d84897f74f6261d8dc7b55c1fa2f21af84
Christian Westhoff <westhoffchristian1@gmail.com>
2021-03-23 18:38:02 +0100

    Bump up packages to latest

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World')
+if (process.env.NODE_ENV !== 'development') {
+  process.on('unhandledRejection', (err) => {
+    console.error(err instanceof Error ? err.message : err)
+    process.exit(-1)
+  })
+}
+;(async () => {
+  try {
+    console.log('Hello World!')
+    process.exit(0)
+  } catch (e) {
+    throw e
+  }
+})()
